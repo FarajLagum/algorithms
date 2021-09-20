e20a37f3f6e324bbedb6c631d977183339b1b6e3
FarajLagum <faraj.lagum@gmail.ocm>
2021-09-20 00:27:06 -0400

    add stacks file

---
+++ stacks.py
@@ -0,0 +1,23 @@
+# Using Lists as Stacks
+# last-in, first-out
+
+stack = [3, 4, 5]
+stack.append(6)
+stack.append(7)
+
+print(stack)
+print()
+
+# stack.pop()
+print(stack.pop())
+
+print(stack)
+print()
+
+stack.pop()
+
+stack.pop()
+
+# stack
+print(stack) 
+print()

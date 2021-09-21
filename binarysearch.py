93434aebc8bf1cac71e81c315f0b6b0b46e49c3e
FarajLagum <faraj.lagum@gmail.ocm>
2021-09-20 23:25:50 -0400

    update the binary search file

--- binarysearch.py
+++ binarysearch.py
@@ -2,23 +2,19 @@ import math
 
 
 def binary_search(array, value):
-    """Your code goes here."""
+    """Iterative Binary Search"""
     l = 0
     r = len(array) - 1
     while l <= r:
-        m = math.floor((l+r)/2)
+        m = math.floor((l+r)/2) # the position of the middle element
         if array[m] < value:
             l = m + 1 
         elif array[m] > value:
             r = m - 1 
         else:
-            return m 
+            return m # return the index of the target value
 
-    return -1
-
-
-
-
+    return -1 # search terminates as unsuccessful (the target value not found).
 
 
 
@@ -32,5 +28,12 @@ test_val3 = 1
 #print(array[6])
 print(binary_search(test_array, test_val1))
 print(binary_search(test_array, test_val2))
-print(binary_search(test_array, test_val3))
 
+
+result = binary_search(test_array, test_val3)
+
+
+if result != -1:
+    print ("Element is present at index {}".format(result))
+else:
+    print ("Element is not present in array")

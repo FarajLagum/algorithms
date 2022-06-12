5df9293c80b4938d04722a4d40999d1765a89bf6
FarajLagum <faraj.lagum@gmail.ocm>
2022-06-11 21:36:35 -0400

    change style

--- GraphAlgorithms/deque_list.py
+++ GraphAlgorithms/deque_list.py
@@ -7,33 +7,34 @@ class Deque:
     def isEmpty(self):
         return self.items == []
 
-    def addRear(self, item):
+    def insert_rear(self, item):
         self.items.append(item)
 
-    def addFront(self, item):
+    def insert_front(self, item):
         self.items.insert(0, item)
 
-    def removeFront(self):
+    def remove__front(self):
         return self.items.pop(0)
 
-    def removeRear(self):
+    def remove_rear(self):
         return self.items.pop()
 
     def size(self):
         return len(self.items)
 
 
-d = Deque()
-print(d.isEmpty())
-d.addRear(8)
-d.addRear(5)
-d.addFront(7)
-d.addFront(10)
-print(d.size())
-print(d.isEmpty())
-d.addRear(11)
-print(d.removeRear())
-print(d.removeFront())
-d.addFront(55)
-d.addRear(45)
-print(d.items)
+if __name__ == "__main":
+    deque_list = Deque()
+    print(deque_list.isEmpty())
+    deque_list.insert_rear(8)
+    deque_list.insert_rear(5)
+    deque_list.insert_front(7)
+    deque_list.insert_front(10)
+    print(deque_list.size())
+    print(deque_list.isEmpty())
+    deque_list.insert_rear(11)
+    print(deque_list.remove_rear())
+    print(deque_list.remove_front())
+    deque_list.insert_front(55)
+    deque_list.insert_rear(45)
+    print(deque_list.items)

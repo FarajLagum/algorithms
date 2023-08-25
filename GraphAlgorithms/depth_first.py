5f78295a21d7804aa6b2360dae4ff0141f15ef7b
FarajLagum <faraj.lagum@gmail.com>
2023-08-25 00:49:01 -0400

    update with commints

--- GraphAlgorithms/depth_first.py
+++ GraphAlgorithms/depth_first.py
@@ -16,12 +16,12 @@ def depth_first(graph, source):
     while len(stack) > 0:
         current = stack.pop()
         print(current)
-        for neighbor in graph[current]:
-            stack.append(neighbor)
+        for neighbor in graph[current]: # graph[current] returns the list of neighbor nodes
+            stack.append(neighbor)  # append is a push to the top of the stack
 
 
 if __name__ == '__main__':
-    depth_first(graph, 'a')
+    depth_first(graph, 'a') # out: abdfce
 
 
 # https://www.youtube.com/watch?v=tWVWeAqZ0WU&t=1333s

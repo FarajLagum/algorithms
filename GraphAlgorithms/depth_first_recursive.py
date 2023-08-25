13b210b718b05181d2bd1acf049a440f312eca6e
FarajLagum <faraj.lagum@gmail.com>
2023-08-25 00:47:21 -0400

    update

--- GraphAlgorithms/depth_first_recursive.py
+++ GraphAlgorithms/depth_first_recursive.py
@@ -1,3 +1,5 @@
+# code to implement depth first algorithm
+
 # stach data structure is the best suties for depth_first
 # directed graph
 graph = {
@@ -10,13 +12,14 @@ graph = {
 }
 
 
-def depth_first(graph, source):
-    print(source)
-    for neighbor in graph[source]:
+def depth_first(graph, source_node):
+    print(source_node) # print the starting node
+    for neighbor in graph[source_node]:
         depth_first(graph, neighbor)
 
 
 if __name__ == '__main__':
+    # print all nodes in the graph according to depth first algorithm
     depth_first(graph, 'a')
 
 # https://www.youtube.com/watch?v=tWVWeAqZ0WU&t=1333s

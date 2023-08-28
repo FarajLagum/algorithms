704a0ed4398b6f43eff8f46c4d5c759334bc092b
FarajLagum <faraj.lagum@gmail.com>
2023-08-28 18:53:51 -0400

    add connected compoents problem

---
+++ GraphAlgorithms/connected_components_count.py
@@ -0,0 +1,62 @@
+test1 ={
+  '0': ['8', '1', '5'],
+  '1': ['0'],
+  '5': ['0', '8'],
+  '8': ['0', '5'],
+  '2': ['3', '4'],
+  '3': ['2', '4'],
+  '4': ['3', '2']
+} # answer=2
+
+test2 ={
+  1: [2],
+  2: [1,8],
+  6: [7],
+  9: [8],
+  7: [6, 8],
+  8: [9, 7, 2]
+} # answer=1
+
+
+test3 = {}
+
+
+test_4 = {
+  0: [4,7],
+  1: [],
+  2: [],
+  3: [6],
+  4: [0],
+  6: [3],
+  7: [0],
+  8: []
+} # answer -> 5
+
+def connected_components_count(graph):
+    visited = set()
+    counter = 0
+
+    for node in graph.keys():
+        #print(node)
+        if explore_nodes(graph, node, visited) == True:
+            counter += 1
+    return counter
+
+
+
+def explore_nodes(graph, current, visited):
+    if current in visited:
+        return False
+    visited.add(current)
+
+    for neighbor in graph[current]:
+        explore_nodes(graph, neighbor, visited)
+
+    return True
+
+print(connected_components_count(test3))
+
+
+
+# https://www.youtube.com/watch?v=tWVWeAqZ0WU&t=1333s
+# https://structy.net/problems/connected-components-count

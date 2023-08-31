31408d6072db4b79585fd52cd03a72e29da6e2f5
FarajLagum <faraj.lagum@gmail.com>
2023-08-31 11:20:34 -0400

    update

---
+++ BinaryTree/BinaryTreeNodeClass.py
@@ -0,0 +1,20 @@
+class Node():
+    def __init__(self, value) -> None:
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+if __name__ == "__main__":
+    a = Node('a')
+    b = Node('b')
+    c = Node('c')
+    d = Node('d')
+    e = Node('e')
+    f = Node('f')
+
+    a.left = b
+    a.right = c
+    b.left = d
+    b.right = e
+    c.right = f

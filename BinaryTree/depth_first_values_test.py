20efaa3df291d3389bcdc2ae896f501d1e855edf
FarajLagum <faraj.lagum@gmail.com>
2023-08-31 11:33:23 -0400

    update tests

--- BinaryTree/depth_first_values_test.py
+++ BinaryTree/depth_first_values_test.py
@@ -2,6 +2,15 @@ from depth_first_values import depth_first_values
 from BinaryTreeNodeClass import Node
 
 
+def test_depth_first_values_empty_tree():
+    assert depth_first_values(None) == []
+
+
+def test_depth_first_values_single_node():
+    root = Node('a')
+    assert depth_first_values(root) == ['a']
+
+
 def test_00():
 
     a = Node('a')
@@ -23,3 +32,59 @@ def test_00():
     # d   e     f
 
     assert depth_first_values(a) == ['a', 'b', 'd', 'e', 'c', 'f']
+
+
+def test_01():
+    a = Node('a')
+    b = Node('b')
+    c = Node('c')
+    d = Node('d')
+    e = Node('e')
+    f = Node('f')
+    g = Node('g')
+    a.left = b
+    a.right = c
+    b.left = d
+    b.right = e
+    c.right = f
+    e.left = g
+    #      a
+    #    /   \
+    #   b     c
+    #  / \     \
+    # d   e     f
+    #    /
+    #   g
+
+    result = depth_first_values(a)
+    assert result == ['a', 'b', 'd', 'e', 'g', 'c', 'f']
+
+
+def test_02():
+    a = Node('a')
+
+    assert depth_first_values(a) == ['a']
+
+
+def test_03():
+    a = Node('a')
+    b = Node('b')
+    c = Node('c')
+    d = Node('d')
+    e = Node('e')
+    a.right = b
+    b.left = c
+    c.right = d
+    d.right = e
+
+    #      a
+    #       \
+    #        b
+    #       /
+    #      c
+    #       \
+    #        d
+    #         \
+    #          e
+
+    assert depth_first_values(a) == ['a', 'b', 'c', 'd', 'e']

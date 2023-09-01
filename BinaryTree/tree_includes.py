382b2d8d86e671f94837c4cb954eb025e483b5a0
FarajLagum <faraj.lagum@gmail.com>
2023-09-01 09:35:39 -0400

    Write a function, tree_includes, that takes in the root of a binary tree and a target value. The function should return a boolean indicating whether or not the value is contained in the tree.

---
+++ BinaryTree/tree_includes.py
@@ -0,0 +1,85 @@
+from BinaryTreeNodeClass import Node
+from collections import deque
+
+'''
+Write a function, tree_includes, that takes in the root of a binary tree and a target value. 
+The function should return a boolean indicating whether or not the value is contained in the tree.
+'''
+
+
+def tree_includes_depth_first_iterative(current, target):
+    if not current:
+        return False
+
+    stack = [current]
+
+    while stack:
+        if current.value == target:
+            return True
+
+        if current.right is not None:
+            stack.append(current.right)
+        if current.left is not None:
+            stack.append(current.left)
+
+        current = stack.pop()
+
+    return False
+
+
+def tree_includes_depth_first_recursive(root, target):
+    if not root:
+        return False
+
+    if root.val == target:
+        return True
+
+    return tree_includes_depth_first_recursive(root.left, target) or tree_includes_depth_first_recursive(root.right, target)
+
+
+def tree_includes_breadth_first(root, target):
+    if not root:
+        return False
+
+    queue = deque([root])
+
+    while queue:
+        node = queue.popleft()
+
+        if node.val == target:
+            return True
+
+        if node.left:
+            queue.append(node.left)
+
+        if node.right:
+            queue.append(node.right)
+
+    return False
+
+
+def test_00():
+    a = Node("a")
+    b = Node("b")
+    c = Node("c")
+    d = Node("d")
+    e = Node("e")
+    f = Node("f")
+
+    a.left = b
+    a.right = c
+    b.left = d
+    b.right = e
+    c.right = f
+
+    #      a
+    #    /   \
+    #   b     c
+    #  / \     \
+    # d   e     f
+
+    return tree_includes_depth_first_iterative(a, "e")  # --> True
+
+
+if __name__ == "__main__":
+    print(test_00())
---
+++ BinaryTree/tree_includes_test.py
@@ -0,0 +1,133 @@
+from BinaryTreeNodeClass import Node
+from tree_includes import tree_includes_depth_first_iterative
+
+
+def test_00():
+    a = Node("a")
+    b = Node("b")
+    c = Node("c")
+    d = Node("d")
+    e = Node("e")
+    f = Node("f")
+
+    a.left = b
+    a.right = c
+    b.left = d
+    b.right = e
+    c.right = f
+
+    #      a
+    #    /   \
+    #   b     c
+    #  / \     \
+    # d   e     f
+
+    assert tree_includes_depth_first_iterative(a, "e") == True
+
+
+def test_01():
+    a = Node("a")
+    b = Node("b")
+    c = Node("c")
+    d = Node("d")
+    e = Node("e")
+    f = Node("f")
+
+    a.left = b
+    a.right = c
+    b.left = d
+    b.right = e
+    c.right = f
+
+    #      a
+    #    /   \
+    #   b     c
+    #  / \     \
+    # d   e     f
+    assert tree_includes_depth_first_iterative(a, "a") == True
+
+
+def test_02():
+
+    a = Node("a")
+    b = Node("b")
+    c = Node("c")
+    d = Node("d")
+    e = Node("e")
+    f = Node("f")
+
+    a.left = b
+    a.right = c
+    b.left = d
+    b.right = e
+    c.right = f
+
+    #      a
+    #    /   \
+    #   b     c
+    #  / \     \
+    # d   e     f
+
+    assert tree_includes_depth_first_iterative(a, "n") == False
+
+
+def test_03():
+    a = Node("a")
+    b = Node("b")
+    c = Node("c")
+    d = Node("d")
+    e = Node("e")
+    f = Node("f")
+    g = Node("g")
+    h = Node("h")
+
+    a.left = b
+    a.right = c
+    b.left = d
+    b.right = e
+    c.right = f
+    e.left = g
+    f.right = h
+
+    #      a
+    #    /   \
+    #   b     c
+    #  / \     \
+    # d   e     f
+    #    /       \
+    #   g         h
+
+    assert tree_includes_depth_first_iterative(a, "f") == True
+
+
+def test_04():
+    a = Node("a")
+    b = Node("b")
+    c = Node("c")
+    d = Node("d")
+    e = Node("e")
+    f = Node("f")
+    g = Node("g")
+    h = Node("h")
+
+    a.left = b
+    a.right = c
+    b.left = d
+    b.right = e
+    c.right = f
+    e.left = g
+    f.right = h
+
+    #      a
+    #    /   \
+    #   b     c
+    #  / \     \
+    # d   e     f
+    #    /       \
+    #   g         h
+
+    assert tree_includes_depth_first_iterative(a, "p") == False
+
+
+def test_05():
+    assert tree_includes_depth_first_iterative(None, "b") == False

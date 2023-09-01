2a179d3f4958296241c32a1f2a30af69ea2a13b1
FarajLagum <farajlagum@FarajLagums-MacBook-Pro.local>
2023-08-31 22:23:58 -0400

    update

--- CodingProblems/easy/has_pair_with_sum.py
+++ CodingProblems/easy/has_pair_with_sum.py
@@ -16,14 +16,21 @@ def has_pair_with_sum(nums, k):
     num_set = set()
     
     for num in nums:
-        complement = k - num # The complement represents the number that, when added to num, would give the desired sum k.
+        # The complement represents the number that, when added to num, would give the desired sum k.
+        complement = k - num 
+        # if the complement is in the set,  w've found a pair of numbers that add up to the target sum k,
         if complement in num_set:
+            # so the function returns True. 
             return True
         num_set.add(num)
     
     return False
 
-
+'''
+We can use a list instead of the set. However, The set-based solution has an overall time complexity of O(n) 
+due to the fast membership testing, while the list-based solution would have a worst-case time complexity 
+of O(n^2) due to the nested linear searches.
+'''
 if __name__ == "__main__":
     
     numbers = [10, 15, 3, 7]

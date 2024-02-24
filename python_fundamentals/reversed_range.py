2088fdd1749c7c9fa468f44fa1bd22528aea984a
FarajLagum <farajlagum@alpha.ht.home>
2024-02-24 18:43:28 -0500

    more python findamentals

---
+++ python_fundamentals/reversed_range.py
@@ -0,0 +1,17 @@
+n = 5
+
+print("reversed(range(n))")
+
+for i in reversed(range(0, n)):
+    print(i)
+
+print("range(n - 1, -1, -1)")
+for i in range(n - 1, -1, -1):
+    print(i)
+
+
+print("============")
+for i in range(n):
+    print(i)
+
+
